199bc5f9f2c45c77b464f8588ff9ec3ae7579fb9
Justin Priday <jpriday@gmail.com>
2015-12-09 10:46:32 +0200

    Trailer and Review data models implemented

---
+++ app/src/main/java/com/justinpriday/nonodegree/projectone1/models/MovieReviewData.java
@@ -0,0 +1,63 @@
+package com.justinpriday.nonodegree.projectone1.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.justinpriday.nonodegree.projectone1.util.MDBConsts;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by justin on 15/12/09.
+ */
+public class MovieReviewData implements Parcelable {
+
+    private static final String LOG_TAG = MovieReviewData.class.getSimpleName();
+
+    public String id;
+    public String reviewAuthor;
+    public String reviewContent;
+
+    protected MovieReviewData(Parcel in) {
+        this.id = in.readString();
+        this.reviewAuthor = in.readString();
+        this.reviewContent = in.readString();
+    }
+
+    public MovieReviewData(JSONObject inJSON) {
+        try {
+            this.id = inJSON.getString(MDBConsts.MOVIE_REVIEW_ID);
+            this.reviewAuthor = inJSON.getString(MDBConsts.MOVIE_REVIEW_AUTHOR);
+            this.reviewContent = inJSON.getString(MDBConsts.MOVIE_REVIEW_CONTENT);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG,e.getMessage(),e);
+            e.printStackTrace();
+        }
+    }
+
+    public static final Creator<MovieReviewData> CREATOR = new Creator<MovieReviewData>() {
+        @Override
+        public MovieReviewData createFromParcel(Parcel in) {
+            return new MovieReviewData(in);
+        }
+
+        @Override
+        public MovieReviewData[] newArray(int size) {
+            return new MovieReviewData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.reviewAuthor);
+        dest.writeString(this.reviewContent);
+    }
+}
---
+++ app/src/main/java/com/justinpriday/nonodegree/projectone1/models/MovieTrailerData.java
@@ -0,0 +1,79 @@
+package com.justinpriday.nonodegree.projectone1.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.justinpriday.nonodegree.projectone1.util.MDBConsts;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by justin on 15/12/09.
+ */
+public class MovieTrailerData implements Parcelable {
+
+    private static final String LOG_TAG = MovieTrailerData.class.getSimpleName();
+
+    public String id; //Unique String ID of video from MDB
+    public String trailerTitle;
+    public String trailerKey; //Unique Identifier of video on site
+    public int trailerSite; //Internal Key for site, based on URL parsing support. Unsupported sites rejected at API receipt stage.
+    //1=Youtube
+
+
+    protected MovieTrailerData(Parcel in) {
+        this.id = in.readString();
+        this.trailerTitle = in.readString();
+        this.trailerKey = in.readString();
+        this.trailerSite = in.readInt();
+    }
+
+    public MovieTrailerData(JSONObject inJSON) {
+        try {
+            this.id = inJSON.getString(MDBConsts.MOVIE_TRAILER_ID);
+            this.trailerTitle = inJSON.getString(MDBConsts.MOVIE_TRAILER_TITLE);
+            this.trailerKey = inJSON.getString(MDBConsts.MOVIE_TRAILER_KEY);
+            this.trailerSite = MDBConsts.GET_SITE_ID(inJSON.getString(MDBConsts.MOVIE_TRAILER_SITE));
+        } catch (JSONException e) {
+            Log.e(LOG_TAG,e.getMessage(),e);
+            e.printStackTrace();
+        }
+    }
+
+    public static final Creator<MovieTrailerData> CREATOR = new Creator<MovieTrailerData>() {
+        @Override
+        public MovieTrailerData createFromParcel(Parcel in) {
+            return new MovieTrailerData(in);
+        }
+
+        @Override
+        public MovieTrailerData[] newArray(int size) {
+            return new MovieTrailerData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.trailerTitle);
+        dest.writeString(this.trailerKey);
+        dest.writeInt(this.trailerSite);
+    }
+
+    public String getTrailerThumbURL() {
+        switch (this.trailerSite) {
+            case 1:
+                return MDBConsts.MOVIE_TRAILER_YOUTUBE_THUMBNAIL_URL + this.trailerKey + "/"
+                        + MDBConsts.MOVIE_TRAILER_YOUTUBE_THUMBNAIL_VERSION;
+            default:
+                return null;
+        }
+    }
+}
